14b5028dc2754461fd4cbb6708dd0d561391bf98
GavinBurgess <gavin.burgess7@gmail.com>
2021-06-08 22:29:12 +0200

    Display getAllCharacters network call response on MainActivity

--- app/src/main/java/com/e/ramapp/MainActivity.kt
+++ app/src/main/java/com/e/ramapp/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.e.ramapp
 
+import android.os.ProxyFileDescriptorCallback
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.e.ramapp.data.dto.AllCharactersDto
+import com.e.ramapp.data.repos.RaMRepo
+import kotlinx.coroutines.launch
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.activityViewModels
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MyViewModel : ViewModel() {
+    fun getData(callback: (String)-> Unit) {
+        viewModelScope.launch {
+            kotlin.runCatching {
+                RaMRepo().getAllCharacters()
+            }.onSuccess {
+                callback(it.toString())
+            }.onFailure {
+                callback(it.toString())
+            }
+        }
+    }
+}
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewModel = ViewModelProvider(this).get(MyViewModel::class.java)
+        viewModel.getData {
+            txtView.text = it.toString()
+        }
+
     }
+
 }
